5b4085e2265143bd3c27ca86c13598fc95b30350
Christoph Wagner <chwagner@gmail.com>
2014-07-02 19:04:50 -0700

    Initial checkin

---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+  summary: "A library to create mobile shelf-style menus"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files(['./lib/snap.css', './lib/snap.js'], 'client');
+});
